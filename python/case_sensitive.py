469e6926e222d55e963fdf1fe7ea9b233990435d
Yangxi9579 <156685398+Yangxi9579@users.noreply.github.com>
2024-01-22 15:15:10 +0000

    Update case_sensitive.py

--- python/case_sensitive.py
+++ python/case_sensitive.py
@@ -1,1 +1,11 @@
-
+def count_case(s):
+    lowcount = 0
+    upcount = 0
+    for i in s:
+        if i >= 'a' and i <= 'z':
+            lowcount = lowcount+1
+        elif i >='A' and i <= 'Z':
+            upcount = upcount+1
+    print(f"There have {lowcount} lowcount, and {upcount} upcount.")
+xxx=input("Please enter text:")
+count_case(xxx)
